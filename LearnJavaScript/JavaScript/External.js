f761b28bab8fecd5dcdaa3abac4df7ad566325b4
NonthawatKRT <nonnon48265566@gmail.com>
2024-10-30 21:54:44 +0700

    update version

---
+++ LearnJavaScript/JavaScript/External.js
@@ -0,0 +1,7 @@
+function turnOnLight() {
+    document.getElementById('myImage').src = 'images/pic_bulbon.gif';
+}
+
+function turnOffLight() {
+    document.getElementById('myImage').src = 'images/pic_bulboff.gif';
+}
